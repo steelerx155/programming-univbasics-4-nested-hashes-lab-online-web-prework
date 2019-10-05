5902fff24b809e6120519a27f03116dba27bc5a0
Randy Steele <steelerx155@gmail.com>
2019-10-05 19:29:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,8 +120,8 @@ def adding_to_dennis
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-          :1 => "Assembly"
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+end	  programmer_hash
 end
