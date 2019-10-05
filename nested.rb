0091f62b92ca3a5e9f03a7b04040ab37ba70bf66
Randy Steele <steelerx155@gmail.com>
2019-10-05 18:56:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-   programmer_hash[:grace_hopper][:alan_kay][:dennis_ritchie][:yukihiro_matsumoto]
+   programmer_hash[:grace_hopper][:alan_kay][:dennis_ritchie][:adding_matz][:yukihiro_matsumoto]
     
 end
 
