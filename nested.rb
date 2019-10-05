3b858d0e8a83e493db360bcfbe54e41b3ada419b
Randy Steele <steelerx155@gmail.com>
2019-10-05 18:28:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,6 +6,7 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
                   },
+                  puts [grace_hopper]
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
